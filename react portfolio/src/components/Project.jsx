cebef7a87036f7f53b49842c143f2057c41b6314
Eth <ethtru@gmail.com>
2023-11-19 13:59:22 -0500

    added background images to two of the projects in project component

--- react portfolio/src/components/Project.jsx
+++ react portfolio/src/components/Project.jsx
@@ -13,18 +13,19 @@ export default function Project() {
           />
         </a> */}
         <div id="pic-section">
-          <a href="https://github.com/ethtru/Matter-of-Taste">
-            <div class="sub-pic">
+          <a href="https://matter-of-taste-2cb992252e34.herokuapp.com/">
+            <div class="sub-pic"  id="matter-of-taste">
               <h4>Matter of Taste</h4>
+              
             </div>
           </a>
-          <a href="https://github.com/ethtru/Weather-Dashboard">
-            <div class="sub-pic">
+          <a href="https://ethtru.github.io/Weather-Dashboard/">
+            <div class="sub-pic" id="weather">
               <h4>Weather Dashboard</h4>
             </div>
           </a>
-          <a href="https://github.com/ErinHamrick/tasty-jams">
-            <div class="sub-pic">
+          <a href="https://erinhamrick.github.io/tasty-jams/">
+            <div class="sub-pic" id="tasty-jams">
               <h4>Tasty Jams</h4>
             </div>
           </a>
