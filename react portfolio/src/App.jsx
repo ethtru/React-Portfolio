524ba656b268daee6bcf39cdbad384960372191b
Eth <ethtru@gmail.com>
2023-11-23 10:43:27 -0500

    got the footer to finally align at the bottom

--- react portfolio/src/App.jsx
+++ react portfolio/src/App.jsx
@@ -8,11 +8,11 @@ function App() {
 
 
   return (
-    <>
+    <div className='app-container'>
     <Header/>
     <Outlet/>
     <Footer/>
-    </>
+    </div>
   )
 }
 
--- react portfolio/src/components/Footer.jsx
+++ react portfolio/src/components/Footer.jsx
@@ -1,11 +1,11 @@
 export default function Footer() {
     return (
         <>
-       <footer>
+       <footer class='footer'>
          
         {/* <h2 id="contactme">Contact Me</h2> */}
         
-          <p><a href="#"></a>ethtru@gmail.com</p>
+          <p><a href="ethtru@gmail.com"></a>ethtru@gmail.com</p>
           <p>865-773-7500</p>
           <p>
             <a class="no-decoration" href="https://github.com/ethtru">GitHub</a>
