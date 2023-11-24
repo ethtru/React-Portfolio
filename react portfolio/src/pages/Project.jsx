8f3be712f4d6161943aa004c52f3893e15903c41
Eth <ethtru@gmail.com>
2023-11-24 13:48:16 -0500

    added background images to work section and styled resume section

--- react portfolio/src/pages/Project.jsx
+++ react portfolio/src/pages/Project.jsx
@@ -1,10 +1,10 @@
-import Footer from "../components/Footer"
+import Footer from "../components/Footer";
 
 export default function Project() {
-    return (
-        <>
-        <h1>Work</h1>
-        {/* <a href="https://ethtru.github.io/first-challenge/">
+  return (
+    <>
+      <h1>Work</h1>
+      {/* <a href="https://ethtru.github.io/first-challenge/">
           <h3>Horiseon</h3>
           <img
             id="hero-pic"
@@ -12,30 +12,28 @@ export default function Project() {
             alt="Horiseon Website"
           />
         </a> */}
-        <div id="pic-section">
-          <a href="https://matter-of-taste-2cb992252e34.herokuapp.com/">
-            <div class="sub-pic"  id="matter-of-taste">
-              <h4>Matter of Taste</h4>
-              
-            </div>
-          </a>
-          <a href="https://ethtru.github.io/Weather-Dashboard/">
-            <div class="sub-pic" id="weather">
-              <h4>Weather Dashboard</h4>
-            </div>
-          </a>
-          <a href="https://erinhamrick.github.io/tasty-jams/">
-            <div class="sub-pic" id="tasty-jams">
-              <h4>Tasty Jams</h4>
-            </div>
-          </a>
-          <a href="https://et-note-taker-f273f1d6e088.herokuapp.com/notes">
-            <div class="sub-pic">
-              <h4>Note Taker</h4>
-            </div>
-          </a>
-        </div>
-   
-      </>
-    )
+      <div id="pic-section">
+        <a href="https://matter-of-taste-2cb992252e34.herokuapp.com/">
+          <div class="sub-pic" id="matter-of-taste">
+            <h4>Matter of Taste</h4>
+          </div>
+        </a>
+        <a href="https://ethtru.github.io/Weather-Dashboard/">
+          <div class="sub-pic" id="weather">
+            <h4>Weather Dashboard</h4>
+          </div>
+        </a>
+        <a href="https://erinhamrick.github.io/tasty-jams/">
+          <div class="sub-pic" id="tasty-jams">
+            <h4>Tasty Jams</h4>
+          </div>
+        </a>
+        <a href="https://ethtru.github.io/Code-Quiz/">
+          <div class="sub-pic" id="code-quiz">
+            <h4>Code Quiz</h4>
+          </div>
+        </a>
+      </div>
+    </>
+  );
 }
--- react portfolio/src/pages/Resume.jsx
+++ react portfolio/src/pages/Resume.jsx
@@ -1,16 +1,31 @@
 import Footer from "../components/Footer";
 
 export default function Resume() {
-    return (
-      <div>
-        <h1>Resume</h1>
-        <p>
-          <a href="/path/to/your/resume.pdf" download>
-            <button className="btn">
-              <i className="fa fa-download"></i> Download Resume
-            </button>
-          </a>
-        </p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Resume</h1>
+
+      <p>
+        <div>
+          <h2 id="proficiencies">Proficiencies:</h2>
+          <ul>
+            <li id="li">JavaScript</li>
+            <li id="li">CSS</li>
+            <li id="li">HTML</li>
+            <li id="li">Node</li>
+            <li id="li">React</li>
+            <li id="li">MongoDB</li>
+            <li id="li">SQL</li>
+            <li id="li">Express</li>
+            <li id="li">Mongoose</li>
+          </ul>
+        </div>
+        <a href="./public/Resume.pages" download>
+          <button className="btn">
+            <i className="fa fa-download"></i> Download Resume
+          </button>
+        </a>
+      </p>
+    </div>
+  );
+}
